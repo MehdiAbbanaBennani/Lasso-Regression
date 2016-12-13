0b853027cf883252bdfd3a3a4b57452f4e9eee9a
Stevejobsforever <julien_mattei@hotmail.com>
2016-12-13 22:42:05 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,3 +1,33 @@
+##CODE QUI MARCHE CHEZ MOI, JAI PAS REUSSI A FAIRE TOURNER LE TIEN
+train_set = data.matrix(read.table("/Users/Julien/Desktop/Lasso/Base.txt", sep=','))
+
+X = train_set[,2:11]
+y = train_set[,1]
+
+# Question 2 : Multinlinear regression least squares estimator
+
+estimate_beta <- function(X, y){
+  a = t(X)%*%X
+  b= solve(a)
+  c= t(X)%*%y
+  beta = b%*%c
+  return(beta)
+}
+
+estimate_y <- function(X, beta){
+  y_est = X%*%beta
+  return(y_est)
+}
+
+compute_rmse <- function(y, y_est){
+  return(sum((y-y_est)^2)/length(y))
+}
+
+beta = estimate_beta(X=X, y=y)
+y_est = estimate_y(X=X, beta=beta)
+rmse = compute_rmse(y=y, y_est)
+
+#########CODE DE MEHDI##########################################
 # Data importation
 train_set = data.matrix(read.table("Base.txt", sep=','))
 
