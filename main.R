b7d60cd8397c0abec55d37aa77d0cb4b3f901788
MehdiAB161 <mehdi-b-@live.Fr>
2017-01-02 20:07:49 +0100

    All answered

--- main.R
+++ main.R
@@ -8,6 +8,10 @@ compute_rmse <- function(y, y_est){
   return(sum((y-y_est)^2)/length(y))
 }
 
+count_zeros <- function(x){
+  return(sum(x == 0) )
+}
+
 # Question 2 : Multinlinear regression least squares estimator
 
 # Estimate least sqaure OLS regression estimate of beta
@@ -46,6 +50,8 @@ rmse_lse_train = compute_rmse(y=y_train, y_est=y_est_train)
 y_est_test = predict_y(X=X_test, beta=beta_hat)
 rmse_lse_test = compute_rmse(y=y_test, y_est=y_est_test)
 
+nb_zeros_ols = count_zeros(beta_hat)
+
 # Question 7 : Estimateur Lasso
 
 # Package
@@ -53,23 +59,34 @@ install.packages("glmnet")
 library("glmnet")
 
 # Parameters
-lambda = 0.1
+lambda = c(2)
 
 # Importing the dataset
 lasso_data_set = data.matrix(read.table("mydata.txt", sep=','))
-X = lasso_data_set[, 2:ncol(lasso_data_set)]
-y = lasso_data_set[, 1]
+
+# Computing the train-test slicing coordinates
+train_row_limit = train_set_size * nrow(lasso_data_set)
+test_row_start = train_row_limit + 1
+
+# Splitting the data set into a train set and test set
+X_train = lasso_data_set[1:train_row_limit, 2:ncol(lasso_data_set)]
+y_train = lasso_data_set[1:train_row_limit, 1]
+X_test = lasso_data_set[test_row_start:nrow(lasso_data_set), 2:ncol(lasso_data_set)]
+y_test = lasso_data_set[test_row_start:nrow(lasso_data_set), 1]
 
 # Estimating the lasso regression parameters using the glmnet package
 # The parameter alpha is the weight between Lasso and Ridge regression, we set it to one for Lasso regression
-fit = glmnet(x=X, y=y, alpha = 1, lambda = lambda)
+fit = glmnet(x=X_train, y=y_train, alpha = 1, lambda = lambda)
 
 # Predicting y using the lasso estimated parameters
-y_est_lasso = predict(fit, newx = X, type = "response", s=lambda)
+y_est_lasso = predict(fit, newx = X_test, type = "response", s=lambda)
 
 # Computing the score for the lasso regression
-rmse_lasso = compute_rmse(y_est_lasso, y)
+rmse_lasso = compute_rmse(y_est_lasso, y_test)
 
+# Conting the number of zeros
+nb_non_zeros_ols = fit$df
+nb_zeros_ols = ncol(lasso_data_set) - nb_non_zeros_ols
 
 # Question 10 : Cross Validation
 
@@ -92,9 +109,10 @@ X_test = lasso_data_set[test_row_start:nrow(lasso_data_set), 2:ncol(lasso_data_s
 y_test = lasso_data_set[test_row_start:nrow(lasso_data_set), 1]
 
 rmse_min = Inf
-lambda_min = 0
+lambda_min = c(0)
 # Estimating the lasso regression parameters using the glmnet package
 for (lambda in lambda_array){
+  lambda = c(lambda)
   # The parameter alpha is the weight between Lasso and Ridge regression, we set it to one for Lasso regression
   fit = glmnet(x=X_train, y=y_train, alpha = 1, lambda = lambda)
   # Predicting y using the lasso estimated parameters
@@ -116,6 +134,20 @@ y_est_lasso = predict(fit, newx = X_test, type = "response", s=lambda)
 # Computing the score for the lasso regression
 rmse_lasso = compute_rmse(y_est_lasso, y_test)
 
-# Alternative method using the glmnet package
+
+
+# Alternative method for Cross Validation using the glmnet package
 # Cross validation with 100 lambda trials
-fit = glmnet(x, y, alpha = 1, nlambda = 100)
+fit_auto = glmnet(X_train, y_train, alpha = 1, nlambda = 100)
+
+# Predicting y using the lasso estimated parameters
+y_est_lasso_auto = predict(fit, newx = X_test, type = "response", s=lambda)
+
+# Computing the score for the lasso regression
+rmse_lasso_auto = compute_rmse(y_est_lasso_auto, y_test)
+
+# The obtained value is slightly inferior to the value we obtained manually
+
+# Plotting the number of zeros against lambdas
+nb_zeros_array = ncol(lasso_data_set) - fit_auto$df
+lambdas_array = fit_auto$lambda
